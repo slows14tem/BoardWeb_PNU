194d0a76ea5a280790f200d2b6e4e628db5da2bf
slows14tem <slows14@naver.com>
2022-12-28 00:35:11 +0900

    2022-12-28

--- BoardWeb/src/main/java/com/rubypaper/board/persistence/CommentRepository.java
+++ BoardWeb/src/main/java/com/rubypaper/board/persistence/CommentRepository.java
@@ -10,8 +10,9 @@ import com.rubypaper.board.domain.Comment;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
 	
+	//findBy 외래키(fk) 조회방법
+	//	findBy + fk를 관리하는 entity필드명에서 첫글자 대문자 + _ + fk entity 식별자 필드명에서 첫글자 대문자
+	//https://pooney.tistory.com/83
 	Page<Comment> findByBoard_Seq(Long i, Pageable pageable);
 
-//	@Query("SELECT b FROM Comment b")
-//	Page<Comment> getCommentList(Pageable pageable);
 }
